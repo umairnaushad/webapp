6ef45a3135d4b60e3a8f01489b6fdf7bda05ca54
umair.naushad <umair.naushad@emaratech.ae>
2021-05-26 16:44:23 +0400

    Delete working wih form data

--- backend-python-flask-api/app.py
+++ backend-python-flask-api/app.py
@@ -1,6 +1,7 @@
 import sys
 
 from flask import Flask, request, flash, url_for, redirect, render_template, jsonify
+from sqlalchemy.sql.functions import user
 from base import Session, engine, Base
 from user import User
 from config import DATABASE_URI
@@ -28,10 +29,8 @@ def register():
 
 @app.route('/register', methods = ['GET', 'POST'])
 def new():
-   req = request.form
-   username = req.get("username")
-   email = req.get("email")
-   print("username="+str(username)+"-email="+str(email))
+   username = request.form.get("username")
+   email = request.form.get("email")
    if request.method == 'POST':
       if not username or not email:
          flash('Please enter all the fields', 'error')
@@ -41,7 +40,6 @@ def new():
          session.commit()
          flash('Record was successfully added')
          return render_template('user.html', users=user, isList="false")
-         #return redirect(url_for('user'))
    return render_template('register.html')
 
 
@@ -61,61 +59,40 @@ def getUserByName(name):
    if request.method == 'GET':
       return getUserByUsername(name)
 
+##################################
+
+@app.route('/api/v1/resources/users/<int:user_id>/delete/', methods=['POST'])
+def deleteUser(user_id):
+    if request.method == 'POST':
+      return deleteUser(user_id)
+
+@app.route("/api/v1/resources/users/<int:user_id>/update/", methods=['POST'])
+def updateUser(user_id):
+    updateUser = session.query(User).filter_by(id=user_id).one()
+    if request.method == 'POST':
+        if request.form['email']:
+            updateUser.email = request.form['email']
+            return redirect(url_for('getAllUsers'))
+
+
 ####################
 
 
 @app.route('/api/v1/resources/users/users/create', methods=['POST'])
 def createUser():
    if request.method == 'POST':
-      session.add(User(request.form.get("username"), request.form.get("email")))
-      session.commit()
+      print("a="+str(request.form.get("username")))
+      addNewUser(request.form.get("username"), request.form.get("email"))
       return redirect(url_for('getAllUsers'))
-
-'''
-@app.route('/register', methods = ['GET', 'POST'])
-def new():
-   req = request.form
-   username = req.get("username")
-   email = req.get("email")
-   if request.method == 'POST':
-      if not username or not email:
-         flash('Please enter all the fields', 'error')
-      else:
-         user = User(username, email)
-         session.add(user)
-         session.commit()
-         flash('Record was successfully added')
-         return render_template('user.html', users=user, isList="false")
-         #return redirect(url_for('user'))
-   return render_template('register.html')
-'''
-
-@app.route('/')
-@app.route('/userApi', methods = ['GET', 'POST'])
-def booksFunction():
-   req = request.form
-   username = req.get("username")
-   email = req.get("email")
-   if request.method == 'GET':
-      return getUsers()
-   elif request.method == 'POST':
-      print("POST")      
-      print("username=" + str(username))
-      print("email=" + str(email))
-      newUser = User(username, email)
-      session.add(newUser)
-      session.commit()
-      #return jsonify(User=newUser.serialize)
-      return render_template('user.html', users=newUser, isList="false")
-
-
 
 
 def addNewUser(userName, email):
-   newUser = User(username=userName, email=email)
-   session.add(newUser)
+   user = User(username=userName, email=email)
+   session.add(user)
    session.commit()
-   return jsonify(User=newUser.serialize)
+   print("b="+str(user.email))
+   getUserByUsername(userName)
+   return jsonify(User=user.serialize)
 
 ################################
 
@@ -124,12 +101,33 @@ def getUsers():
    return jsonify(users= [user.serialize for user in users])
 
 def getUserById(user_id):
-   users = session.query(User).filter_by(id = user_id).one()
-   return jsonify(users= users.serialize)
+   user = session.query(User).filter_by(id = user_id).one()
+   return jsonify(user= user.serialize)
 
 def getUserByUsername(username):
    users = session.query(User).filter_by(username = username).all()
    return jsonify(users= [user.serialize for user in users])
+
+def updateUser(id, username, email):
+    updatedUser = session.query(User).filter_by(id=id).one()
+    if not username:
+        updatedUser.username = User
+    if not email:
+        updatedUser.email = email
+    session.add(updatedUser)
+    session.commit()
+    return 'Updated User with id %s' % id
+
+def deleteUser(id):
+    userToDelete = session.query(User).filter_by(id=id).one()
+    session.delete(userToDelete)
+    session.commit()
+    return jsonify(
+        status='success',
+        id=userToDelete.id,
+        username=userToDelete.username,
+        email=userToDelete.email
+    ), 200
 
 if __name__ == '__main__':
    Base.metadata.create_all(engine)
