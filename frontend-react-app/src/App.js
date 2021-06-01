1cb7730434a88a45901a0d6e51865ba79c180fd8
Umair Naushad <Umair.Naushad@emaratech.ae>
2021-06-01 07:17:13 +0400

    Successful get

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -4,23 +4,32 @@ class UserProfiles extends React.Component {
   constructor(){
     super();
     this.state = {
-      //name: {title: '', first: '', last: ''},
       user: {id: '', username: '', email: ''}
-      //image: ''
     };
     // fix the this value
     this.getUser = this.getUser.bind(this);
   }
 
   componentWillMount() {
-    this.getUser();
+    this.getUser(12);
   }
 
-  getUser() {
-    //fetch('https://randomuser.me/api/')
-    fetch('http://127.0.0.1:5000/api/v1/resources/users/ById/11')
-    
-    //fetch('http://localhost:5000/query-example?id=20')
+  getUser(userId) {
+    fetch('http://127.0.0.1:5000/api/v1/resources/users/ById/' + userId)
+    .then(response => {
+      if(response.ok) return response.json();
+      throw new Error('Request failed.');
+    })
+    .then(data => {
+      this.setState({user: data.user});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  getAllUsers(userId) {
+    fetch('http://127.0.0.1:5000/api/v1/resources/users/ById/' + userId)
     .then(response => {
       if(response.ok) return response.json();
       throw new Error('Request failed.');
@@ -35,10 +44,10 @@ class UserProfiles extends React.Component {
 
   render() {
     return (
-	<div>
-          <h1>{`${this.state.user.id} ${this.state.user.username} ${this.state.user.email}`}</h1>
-          <button onClick={this.getUser}>Get new user.</button>
-	</div>
+  <div>
+    <h1>{`${this.state.user.id} ${this.state.user.username} ${this.state.user.email}`}</h1>
+    <button onClick={this.getUser}>Get new user.</button>
+  </div>
     );
   }
 }
